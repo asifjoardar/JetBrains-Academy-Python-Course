737a2d703e8cacf382aac8d08ddaec044d94141f
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2020-07-15 20:15:11 +0600

    Create I_am_so_lite.py

---
+++ Project: Simple Banking System/Stage 3 I am so lite/I_am_so_lite.py
@@ -0,0 +1,83 @@
+import math
+import random
+import sqlite3
+conn = sqlite3.connect('card.s3db')
+cur = conn.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS card (id INTEGER, number TEXT, pin TEXT, balance INTEGER DEFAULT 0);')
+conn.commit()
+count = 0
+user_info = {}
+while True:
+    print("1. Create an account\n2. Log into account\n0. Exit")
+    choice = int(input())
+    print("")
+    if choice == 1:
+        #Issuer Identification Number (IIN)
+        random.seed(count)
+        iin = random.randint(400000, 400000)
+        #customer account number (can)
+        random.seed(count)
+        can = random.randint(000000000, 999999999)
+        random.seed(count)
+        pin = random.randint(0000, 9999)
+        #card number (cn)
+        cn = str(iin) + str(can)
+        #Luhn Algorithm in action
+        genarate = []
+        sum = 0
+        for i in cn:
+            genarate.append(int(i))
+        for i in range(0,len(genarate)):
+            if (i%2==0):
+                genarate[i] *= 2
+            if genarate[i]>9:
+                genarate[i] -= 9
+            sum += genarate[i]
+        #print(sum)
+        sum = 10*(math.ceil(sum/10)) - sum
+        checksum = str(sum)
+        cn += checksum
+        user_info[cn] = {}
+        user_info[cn]['pin'] = pin
+        user_info[cn]['balance'] = 0
+        print("Your card has been created\nYour card number:")
+        print(cn)
+        print("Your card PIN:")
+        print(pin)
+        print("")
+        cur.execute(f'INSERT INTO card (id, number, pin) VALUES ({400000}, {cn}, {pin})')
+        conn.commit()
+        count += 1
+    elif choice == 2:
+        print("Enter your card number:")
+        check_cn = str(input())
+        print("Enter your PIN:")
+        check_pin = int(input())
+        print("")
+        try:
+            if (user_info[check_cn]['pin'] == check_pin):
+                print("You have successfully logged in!")
+                print("")
+                while True:
+                    print("1. Balance\n2. Log out\n0. Exit""")
+                    print("")
+                    choice1 = int(input())
+                    if choice1 == 1:
+                        print("Balance: {}".format(user_info[check_cn]['balance']))
+                        print("")
+                    elif choice1 == 2:
+                        print("You have successfully logged out!")
+                        print("")
+                        break
+                    else:
+                        print("Bye!")
+                        exit()
+        except KeyError:
+            print("Wrong card number or PIN!")
+            print("")
+        else:
+            print("Wrong card number or PIN!")
+            print("")
+    else:
+        print("Bye!")
+        exit()
