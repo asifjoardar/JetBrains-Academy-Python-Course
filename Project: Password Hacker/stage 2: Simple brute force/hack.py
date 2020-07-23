b6f9cc383081c4e6db7d34f240f5ec09961dfa1f
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2020-07-23 13:27:16 +0600

    Create hack.py

---
+++ Project: Password Hacker/stage 2: Simple brute force/hack.py
@@ -0,0 +1,30 @@
+import sys
+import socket
+import itertools
+
+args = sys.argv
+
+with socket.socket() as client_socket:
+    hostname = args[1]
+    port = int(args[2])
+    address = (hostname, port)
+    client_socket.connect(address)
+    chr = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    for i in range(1,37):
+        ok = True
+        for j in itertools.product(chr, repeat = i):
+            data = j
+            s = str()
+            for l in data:
+                s = s + str(l)
+            s1 = s
+            data = s.encode()
+            client_socket.send(data)
+            response = client_socket.recv(1024)
+            response = response.decode()
+            if(response == "Connection success!"):
+                print(s1)
+                ok = False
+                break
+        if not ok:
+            break
